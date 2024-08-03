51ee6536f279f103ef0400878159ba8f20fb2a88
Aryan9060 <aryanofficial9060@gmail.com>
2024-08-03 20:45:21 +0545

    to-do-list

---
+++ TO-DO-List/todo.js
@@ -0,0 +1,42 @@
+const inputBox = document.querySelector('#input-box')
+const list = document.querySelector('#list-container')
+const button = document.querySelector('.btn')
+
+console.log(list);
+
+button.addEventListener('click', () => {
+
+    if (inputBox.value === "") {
+        alert("Please enter a task")
+    } else {
+        let li = document.createElement('li')
+        li.innerHTML = inputBox.value
+        list.prepend(li)
+        const span = document.createElement("span")
+        span.innerHTML = "\u00d7"
+        li.appendChild(span)
+    }
+    inputBox.value = ""
+    saveData()
+})
+
+list.addEventListener('click', (e) => {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+        saveData()
+    } else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        saveData()
+    }
+}, false);
+
+let saveData = () => {
+    localStorage.setItem('data', list.innerHTML);
+    console.log('saveData');
+    
+}
+let showTask = () => {
+    list.innerHTML=localStorage.getItem("data")
+    console.log('getData');
+}
+showTask()
